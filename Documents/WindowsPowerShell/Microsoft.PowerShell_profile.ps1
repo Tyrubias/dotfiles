971f1c6b9a6590854f46b8b5d12088effbf969ad
Victor Song <vms2@rice.edu>
2022-06-22 19:36:21 -0500

    Windows Updates

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-Invoke-Expression (&starship init powershell)
+Invoke-Expression (oh-my-posh --init --shell pwsh --config "$(scoop prefix oh-my-posh)\themes\powerlevel10k_modern.omp.json")
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
