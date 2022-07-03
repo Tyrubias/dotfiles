6096f7efafd91d95af2a29808a11e87a7a4cf9e8
Victor Song <vms2@rice.edu>
2022-07-03 05:19:43 -0500

    Fixed deduplication logic

---
+++ dot_config/zshrc.d/99_cleanup.zsh
@@ -0,0 +1,15 @@
+if [ -n "$PATH" ]; then
+  old_PATH=:$PATH
+  PATH=
+  while [ -n "$old_PATH" ]; do
+    x=${old_PATH##*:} # the last remaining entry
+    case $PATH in
+      *:"$x":*) ;;        # already there
+      *) PATH=$x:$PATH ;; # not there yet
+    esac
+    old_PATH=${old_PATH%:*}
+  done
+  PATH=${PATH#:}
+  PATH=${PATH%:}
+  unset old_PATH x
+fi
