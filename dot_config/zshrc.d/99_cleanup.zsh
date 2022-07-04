dba363b8b3fd20a8dd50063307e225e112bec481
Victor Song <vms2@rice.edu>
2022-07-03 19:36:52 -0500

    New deduplication techniques

--- dot_config/zshrc.d/99_cleanup.zsh
+++ dot_config/zshrc.d/99_cleanup.zsh
@@ -1,11 +1,11 @@
-if [ -n "$PATH" ]; then
-  old_PATH=:$PATH
+if [[ -n "${PATH}" ]]; then
+  old_PATH=:${PATH}
   PATH=
-  while [ -n "$old_PATH" ]; do
+  while [[ -n "${old_PATH}" ]]; do
     x=${old_PATH##*:} # the last remaining entry
-    case $PATH in
-      *:"$x":*) ;;        # already there
-      *) PATH=$x:$PATH ;; # not there yet
+    case ${PATH} in
+      *:"${x}":*) ;;        # already there
+      *) PATH=${x}:${PATH} ;; # not there yet
     esac
     old_PATH=${old_PATH%:*}
   done
