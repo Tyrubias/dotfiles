b3ea5abac8198365183d488b3a79c34a59fa5001
Victor Song <vms2@rice.edu>
2023-05-19 01:02:39 -0500

    Updates

--- dot_local/bin/executable_librewolf.sh
+++ dot_local/bin/executable_librewolf.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 set -euo pipefail
+IFS=$'\n\t'
 
 PROFILE_DIR=$(mktemp -d)
 
-/Applications/LibreWolf.app/Contents/MacOS/librewolf -profile "${PROFILE_DIR}" -no-remote -new-instance >/dev/null 2>&1 || true
+function cleanup {
+  rm -rf "${PROFILE_DIR}"
+}
 
-rm -rf "${PROFILE_DIR}"
+trap cleanup EXIT
+
+/Applications/LibreWolf.app/Contents/MacOS/librewolf -profile "${PROFILE_DIR}" -no-remote -new-instance >/dev/null 2>&1 || true
